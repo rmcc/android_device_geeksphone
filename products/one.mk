3fe183935e7fe6dc3c6d7a80baa09280e6ddb18c
rmcc <github@cerqueira.org>
2010-08-25 02:10:51 +0100

    Enable stagefright player
    stagefright h264 is finally functional

--- products/one.mk
+++ products/one.mk
@@ -106,7 +106,6 @@ PRODUCT_DEVICE := geeksphone-one
 PRODUCT_MODEL := Geeksphone ONE
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	media.stagefright.enable-player=false \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
