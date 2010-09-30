416fb900a7668aa07d1af22230d01f3ff5439577
rmcc <github@cerqueira.org>
2010-09-30 19:58:00 +0100

    GPO: Changed name of the prebuilt kernel binary

--- geeksphone-one/AndroidBoard.mk
+++ geeksphone-one/AndroidBoard.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/cupcake-kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/adq-kernel
 endif
 
 ## Keyboard maps
