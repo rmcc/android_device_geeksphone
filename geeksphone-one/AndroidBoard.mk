1a5e02c12e26891105465bd01e4436f6716879bb
rmcc <github@cerqueira.org>
2010-10-10 20:59:34 +0100

    GPO: Compile kernel by default
    Instead of relying on a prebuilt binary, just build the kernel and use the result

--- geeksphone-one/AndroidBoard.mk
+++ geeksphone-one/AndroidBoard.mk
@@ -1,8 +1,21 @@
 LOCAL_PATH := $(call my-dir)
 
+
+## Compile Linux Kernel if available
+KERNEL_DEFCONFIG := adq_defconfig
+
+## Comment this line to use a prebuilt binary
+include kernel/AndroidKernel.mk
+
+## Use prebuilt if kernel source is unavailable
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/adq-kernel
+	TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/adq-kernel
 endif
+
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file) : $(TARGET_PREBUILT_KERNEL) | $(ACP)
+	$(transform-prebuilt-to-target)
 
 ## Keyboard maps
 
@@ -133,11 +146,6 @@ $(file) : $(LOCAL_PATH)/proprietary/libOmxVidEnc.so | $(ACP)
 file := $(TARGET_OUT_KEYCHARS)/stmpe1601.kcm.bin
 ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/stmpe1601.kcm.bin | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
 	$(transform-prebuilt-to-target)
 
 # init files for boot.img
